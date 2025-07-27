8b422373594975e8f3fa5a2f6124d3a9449b2ad4
mayur <mayurpatil5703@gmail.com>
2025-07-27 12:59:05 +0530

    Project setup done

---
+++ src/test/java/com/mayur/Slotify/SlotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayur.Slotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
